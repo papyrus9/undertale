099db74de43d4adff3a7a4e9b8c290866f01759b
papyrus9 <130610544+papyrus9@users.noreply.github.com>
2023-04-12 15:46:37 -0600

    Add files via upload

---
+++ papyrus.js
@@ -0,0 +1,6 @@
+function sans(){
+    document.getElementById("sans").src="https://th.bing.com/th/id/OIP.uqvgBFcZFM-KGkibs8maTAHaFP?w=280&h=198&c=7&r=0&o=5&pid=1.7"
+}
+function papyrus(){
+    document.getElementById("papyrus").src="https://th.bing.com/th/id/OIP.fyvxZPlNC7ii9_IYiIpZbgHaE7?w=294&h=195&c=7&r=0&o=5&pid=1.7"
+}
